b8dae9d126aac566154fd7c314661b2867dfb33a
Souma <101255979+5ouma@users.noreply.github.com>
2025-10-19 02:16:09 +0900

    feat(akari/pkg/spreadsheet): Add domain layer
    Prepare the abstraction of real data operation.

--- akari/pkg/spreadsheet/domain/entity/spreadsheet.go
+++ akari/pkg/spreadsheet/domain/entity/spreadsheet.go
@@ -1,1 +1,16 @@
 package entity
+
+type SheetData struct {
+	SpreadsheetId string
+	SheetName     string
+}
+
+type CellRange struct {
+	SheetData
+	Range string
+}
+
+type GridData struct {
+	CellRange
+	Values [][]string
+}
--- akari/pkg/spreadsheet/domain/service/spreadsheet.go
+++ akari/pkg/spreadsheet/domain/service/spreadsheet.go
@@ -1,1 +1,42 @@
 package service
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/kizuna-org/akari/pkg/spreadsheet/domain/entity"
+)
+
+type SpreadsheetService struct{}
+
+func NewSpreadsheetService() *SpreadsheetService {
+	return &SpreadsheetService{}
+}
+
+func (s *SpreadsheetService) ValidateGridData(gridData *entity.GridData) error {
+	if gridData.SpreadsheetId == "" {
+		return fmt.Errorf("spreadsheet ID is required")
+	}
+	if gridData.SheetName == "" {
+		return fmt.Errorf("sheet name is required")
+	}
+	if len(gridData.Values) == 0 {
+		return fmt.Errorf("values cannot be empty")
+	}
+	return nil
+}
+
+func (s *SpreadsheetService) BuildRange(sheetName, rangeStr string) string {
+	if rangeStr == "" {
+		return sheetName
+	}
+	return fmt.Sprintf("%s!%s", sheetName, rangeStr)
+}
+
+func (s *SpreadsheetService) ParseRange(fullRange string) (sheetName, rangeStr string) {
+	parts := strings.Split(fullRange, "!")
+	if len(parts) == 2 {
+		return parts[0], parts[1]
+	}
+	return fullRange, ""
+}
